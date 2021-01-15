fc7ab22bf30161dbdb271f5dc0c2c9855dfc5cf0
lachlanpye <47025210+lachlanpye@users.noreply.github.com>
2021-01-15 22:14:22 +1300

    Implemented base React and NodeJS plate

---
+++ server.js
@@ -0,0 +1,51 @@
+// Import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+// Create express app
+const app = express();
+
+// Set backend port to env variable or port 5000
+const port = process.env.PORT || 5000;
+
+// Show incoming requests
+app.use((req, res, next) => {
+    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+    next();
+});
+
+// Configure bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+}));
+
+// Configure CORS middleware
+app.use(cors());
+
+// Require Route
+const api = require('./routes/routes');
+
+// Configure app to use routes
+app.use('/api/v1/', api);
+
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+};
+
+// Catch any bad requests
+app.get('*', (req, res) => {
+    res.status(200).json({
+        msg: 'Catch All'
+    });
+});
+
+// Configure our server to listen on the port defiend by our port variable
+app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
